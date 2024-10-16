d888965ff7348b9b34e0274a742d7f705c3e351b
Nikhil-2001 <55666435+Nikhil-2001@users.noreply.github.com>
2024-10-17 02:45:44 +0530

    Completed React Essentials Module 3

--- React Starting/src/App.jsx
+++ React Starting/src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import Header from './components/Header.jsx'
 import CoreConcept from './components/CoreConcepts.jsx'
-import { CORE_CONCEPTS } from './data.js'
+import { CORE_CONCEPTS, EXAMPLES } from './data.js'
+import TabButton from './components/TabButton.jsx'
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState()
+
+  function handleOnSelect(selectedButton) {
+    setSelectedTopic(selectedButton)
+  }
+
   return (
     <div>
       <Header></Header>
@@ -10,11 +18,31 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {CORE_CONCEPTS.map((item) => <CoreConcept key={item.title} {...item}/>)}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected = {selectedTopic === 'components'} onSelect={() => handleOnSelect('components')}>Components</TabButton>
+            <TabButton isSelected = {selectedTopic === 'jsx'} onSelect={() => handleOnSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected = {selectedTopic === 'props'} onSelect={() => handleOnSelect('props')}>Props</TabButton>
+            <TabButton isSelected = {selectedTopic === 'state'} onSelect={() => handleOnSelect('state')}>State</TabButton>
+          </menu>
+          {!selectedTopic ?
+            (<p>Please select a topic</p>) :
+            (
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+                </div>
+            )
+          }
         </section>
       </main>
     </div>
--- React Starting/src/components/Header.jsx
+++ React Starting/src/components/Header.jsx
@@ -3,16 +3,16 @@ import './Header.css'
 
 const random_list = ['Crucial', 'CORE', 'Fundamental']
 function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
+    return Math.floor(Math.random() * (max + 1))
 }
 
 export default function Header() {
     return (<header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {random_list[getRandomInt(2)]} React concepts you will need for almost any app you are
-        going to build! My First One
-      </p>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+            {random_list[getRandomInt(2)]} React concepts you will need for almost any app you are
+            going to build! My First One
+        </p>
     </header>)
-  }
+}
