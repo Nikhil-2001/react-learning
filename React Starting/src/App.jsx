4804f8c2241301b6b3b21723c1057f8652ee36de
Nikhil-2001 <55666435+Nikhil-2001@users.noreply.github.com>
2024-10-17 00:14:47 +0530

    Learning React for the first time

---
+++ React Starting/src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './components/Header.jsx'
+import CoreConcept from './components/CoreConcepts.jsx'
+import { CORE_CONCEPTS } from './data.js'
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ React Starting/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import reactImg from '../assets/react-core-concepts.png'
+import './Header.css'
+
+const random_list = ['Crucial', 'CORE', 'Fundamental']
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+export default function Header() {
+    return (<header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {random_list[getRandomInt(2)]} React concepts you will need for almost any app you are
+        going to build! My First One
+      </p>
+    </header>)
+  }
