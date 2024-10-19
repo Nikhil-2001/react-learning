2d0ab8022334ae884b41beb61957cef25e047d98
Nikhil-2001 <55666435+Nikhil-2001@users.noreply.github.com>
2024-10-19 13:19:00 +0530

    Tic Tac Toe Learning

---
+++ 04 07-tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export default function Player({initialName, symbol, isActive, onChangeName})
+{
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    // ImportantConcept
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+        if(isEditing){
+            onChangeName(symbol,playerName)
+        }
+    }
+
+    function handleChange(e) {
+        setPlayerName(e.target.value);
+    }
+    
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+    
+    if(isEditing) {
+        editablePlayerName = <input type="text" value={playerName} required onChange={handleChange}></input>
+    }
+    
+    return (
+        <li className={isActive ? "active" : undefined}>
+          <span className="player">{editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing?'Save':'Edit'}</button>
+      </li>
+    )
+}
