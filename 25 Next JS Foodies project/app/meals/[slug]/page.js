c01645d2ba576edebce9b34c64b494029021f672
Nikhil-2001 <55666435+Nikhil-2001@users.noreply.github.com>
2024-11-12 14:38:26 +0530

    Learnt app router of NextJS

--- 25 Next JS Foodies project/app/meals/[slug]/page.js
+++ 25 Next JS Foodies project/app/meals/[slug]/page.js
@@ -1,10 +1,53 @@
-import Link from "next/link";
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-export default function MealDetailsPage() {
-    return (
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+export async function generateMetadata({params}) {
+  const {slug} = await params
+  console.log(slug)
+  const meal = await getMeal(slug)
+  console.log('Here in generate meadat')
+  if(!meal)
+    notFound()
+  return {
+    title : meal.title,
+    description : meal.summary
+  }
+}
+
+export default async function MealDetailsPage({ params }) {
+  const {slug} = await params
+  const meal = await getMeal(slug);
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
       <main>
-        <h1>This is meal details page</h1>
-        <Link href='/'>Home</Link>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
       </main>
-    );
+    </>
+  );
 }
--- 25 Next JS Foodies project/components/main-header.js
+++ 25 Next JS Foodies project/components/main-header.js
@@ -1,20 +1,22 @@
 import Link from "next/link";
 import logoimg from '@/assets/logo.png'
 import classes from './main-header.module.css'
+import Image from "next/image";
+import NavLink from "./navlink/navlink";
 
 export default function MainHeader() {
     return (<header className={classes.header}>
         <Link className={classes.logo} href='/'>
-            <img src={logoimg.src} alt='A food on a plate wit it'/>
+            <Image src={logoimg} alt='A food on a plate wit it' priority/>
         </Link>
 
         <nav className={classes.nav}>
             <ul>
                 <li>
-                    <Link href='/meals'>Browse Meals</Link>
+                    <NavLink href='/meals'>Browse Meals</NavLink>
                 </li>
                 <li>
-                    <Link href='/community'>Foodies Community</Link>
+                    <NavLink href='/community'>Foodies Community</NavLink>
                 </li>
             </ul>
         </nav>
